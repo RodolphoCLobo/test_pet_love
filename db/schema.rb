99c5a98600832b5a908657e0b658d92792d70426
rodolpholobo <ro@speediomail.com>
2019-08-31 00:35:48 -0300

    Feat: create model Breed and tests.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_161244) do
+ActiveRecord::Schema.define(version: 2019_08_31_033000) do
+
+  create_table "breeds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
